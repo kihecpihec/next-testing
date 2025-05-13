3524423d017e60c86dcd34ac55a4984d496d43a1
kihec <hananmesic301@gmail.com>
2025-05-13 12:57:50 +0200

    minor changes

--- next-test/src/app/page.tsx
+++ next-test/src/app/page.tsx
@@ -7,14 +7,13 @@ export default function Home() {
       <div className='relative z-10'>
         <div className='grid grid-rows-[auto_1fr_auto] w-full max-w-4xl items-center justify-items-center gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
           <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
-            <div className="text-xl">kihec</div>
+            <div className='text-xl'>kihec</div>
             <ol className='list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
               <li className='mb-2 tracking-[-.01em]'>
                 Get started by editing{" "}
                 <code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold'>
                   src/app/page.tsx
                 </code>
-                .
               </li>
               <li className='tracking-[-.01em]'>
                 Save and see your changes instantly.
